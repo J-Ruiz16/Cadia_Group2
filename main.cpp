c0071b008416f4421d5fe622b25eb7d55d943b50
THull5 <thull5@ut.utm.edu>
2025-04-05 21:20:38 -0500

    Updated main.cpp to include find features and added cases for it

--- main.cpp
+++ main.cpp
@@ -16,13 +16,17 @@ int main(){
 
   int choice;
   std::string filename;
+  std::cout << "Game Library! Choose an option:" << std::endl;
 
   do {
-    std::cout << "Game Library! Choose an option:" << std::endl;
     std::cout << "1. Read from file" << std::endl;
     std::cout << "2. Write to a file" << std::endl;
     std::cout << "3. Print game library" << std::endl;
     std::cout << "4. Add a game" << std::endl;
+    std::cout << "5. Find by genre" << std::endl;
+    std::cout << "6. Find by title keyword" << std:: endl;
+    std::cout << "7. Delete a game" << std::endl;
+    std::cout << "0. Exit" << std::endl;
 
     std::cout << "Please enter choice: ";
     std::cin >> choice;
@@ -41,13 +45,10 @@ int main(){
       std::cout << "File was written successfully" << std::endl;
       break;
     case 3:
-      std::cout << "Enter a file name: ";
-      std::cin >> filename;
-      l.read_from_file(filename);
       l.print();
       break;
     case 4: {
-      Game g("", "", "", 0.0, 0);
+      Game g("", "", "", 0.0, 0.0, 0);
       std::cout << "Enter game title: ";
       std::getline(std::cin, g.title);
       std::cout << "Enter game publisher: ";
@@ -56,6 +57,8 @@ int main(){
       std::getline(std::cin, g.genre);
       std::cout << "Enter game hours played: ";
       std::cin >> g.hours;
+      std::cout << "Enter game price: ";
+      std::cin >> g.price;
       std::cout << "Enter game year: ";
       std::cin >> g.year;
       std::cin.ignore();
@@ -64,9 +67,39 @@ int main(){
       std::cout << "The game was added" << std::endl;
       break;
     }
+    case 5: {
+      std::string genre;
+      std::cout << "Enter genre: ";
+      std::getline(std::cin, genre);
+      l.find_genre(genre);
+      break;
+    }
+    case 6: {
+      std::string keyword;
+      std::cout << "Enter part of the title to search for: ";
+      std::getline(std::cin, keyword);
+      l.find_game(keyword);
+      break;
+    }
+    case 7: {
+      std::string title;
+      int year;
+      std::cout << "Enter title to delete: ";
+      std::getline(std::cin, title);
+      std::cout << "Enter year: ";
+      std::cin >> year;
+      std::cin.ignore();
+      l.delete_game(title, year);
+      break;
+    }
+    case 0: {
+      std::cout << "Thank you for using our library!" << std::endl;
+      break;
+    }
     default:
       std::cout << "Invalid choice" << std::endl;
+      std::cout << " " << std::endl;
     }
-  }while(choice != 4);
+  }while(choice != 0);
   return 0;
 }
